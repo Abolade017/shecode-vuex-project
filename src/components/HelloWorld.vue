33d872118c8748fa9cd58271202c6d9fa039b21f
Adeola tawakaltu Adenike <tawakalituadeola68@gmail.com>
2022-08-16 14:10:36 +0100

    shecode-vuex-project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="hello px-10 py-10">
+      <div class="flex items-center justify-between ">
+        <div class="text-2xl font-bold text-blue-400">logo</div>
+        <div class="flex space-x-6 items-center">
+          <button
+            v-for="tab in tabs"
+            :key="tab"
+            @click="selected = tab"
+            class="text-lg font-bold border-gray-300 rounded-md h-12 w-48"
+            :class="['tab-btn', { active: selected === tab }]"
+          >
+            {{ tab }}
+          </button>
+        </div>
+      </div>
+    </div>
+    <component :is="selected"></component>
   </div>
 </template>
 
 <script>
+import Home from "./tabs/Home.vue";
+import About from "./tabs/About.vue";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    Home,
+    About
+  },
+  data() {
+    return {
+      selected: "Home",
+      tabs: ["Home", "About"],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.active {
+  border-bottom: 3px solid blue;
+  background-color: #fcfcfc;
 }
 </style>
---
+++ src/components/tabs/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="mt-20 px-10">this is home tab</div>
+</template>
