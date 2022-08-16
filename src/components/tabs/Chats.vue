1c3ca5a08d5cd340041fae84cd144ebc3c8b1760
Adeola tawakaltu Adenike <tawakalituadeola68@gmail.com>
2022-08-16 15:58:05 +0100

    add more

--- src/components/tabs/Chats.vue
+++ src/components/tabs/Chats.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="mt-20 pl-10 pr-20">
     <div v-for="chat in 10" :key="chat">
-      <div class="flex items-center justify-between">
-        <div class="flex space-x-6 items-center">
-          <div class="rounded-full h-10 w-10">
-            <img src="" alt="" class="rounded-full h-full w-full">
+      <div class="flex items-center justify-between py-4">
+        <div class="flex space-x-6">
+          <div class=" h-10 w-10">
+            <img src="../../assets/logo.png" alt="" class=" rounded-full h-full w-full">
           </div>
-          <div></div>
+          <div>
+            <p class="text-lg text-gray-700">good morning</p>
+          </div>
         </div>
         <div>10:20 AM</div>
       </div>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,30 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import "@/assets/css/tailwind.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJSdq58FXMKMdjowhQMbyVt9TvPCg6BkY",
+  authDomain: "messagechat-63373.firebaseapp.com",
+  projectId: "messagechat-63373",
+  storageBucket: "messagechat-63373.appspot.com",
+  messagingSenderId: "867435579183",
+  appId: "1:867435579183:web:678ad350cd49531fd543da",
+  measurementId: "G-LXLHMBMMML"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
 const app = createApp(App);
 app.use(store);
 app.use(router);
+// app.use(firebase);
 app.mount('#app');
+
